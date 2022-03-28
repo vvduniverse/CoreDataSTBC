238e656e09d3d11345d3e4bca5007ad5261366af
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-28 12:56:33 +0300

    Done

--- CoreDataSTBC/ContentView.swift
+++ CoreDataSTBC/ContentView.swift
@@ -5,84 +5,117 @@
 //  Created by Vahtee Boo on 27.03.2022.
 //
 
+//
+//  CoreDataBC.swift
+//  SwiftUI-Intermediate
+//
+//  Created by Vahtee Boo on 28.03.2022.
+//
+
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+class CoreDataViewModel: ObservableObject {
+    
+    let container: NSPersistentContainer
+    @Published var savedEntitties = [FruitEntity]()
+    
+    init() {
+        container = NSPersistentContainer(name: "FruitsContainer")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Error loading core data \(error)")
             }
         }
+        fetchFruits()
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    
+    func fetchFruits() {
+        let request = NSFetchRequest<FruitEntity>(entityName: "FruitEntity")
+        
+        do {
+            savedEntitties = try container.viewContext.fetch(request)
+        } catch let error {
+            print("Error fetching. \(error)")
+        }
+    }
+    
+    func addFruit(text: String) {
+        let newFruit = FruitEntity(context: container.viewContext)
+        newFruit.name = text
+        saveData()
+    }
+    
+    func updateFruit(entity: FruitEntity) {
+        let currentName = entity.name ?? ""
+        let newName = currentName + "!"
+        entity.name = newName
+        saveData()
+    }
+    
+    func deleteFruit(indexSet: IndexSet) {
+        guard let index = indexSet.first else { return }
+        let entity = savedEntitties[index]
+        container.viewContext.delete(entity)
+        saveData()
+    }
+    
+    func saveData() {
+        do {
+            try container.viewContext.save()
+            fetchFruits()
+        } catch let error {
+            print("Error savint. \(error)")
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct ContentView: View {
+    
+    @StateObject var vm = CoreDataViewModel()
+    @State var textFieldText = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                
+                HStack {
+                    TextField("Add fruit here...", text: $textFieldText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .shadow(color: .gray, radius: 5, x: 5, y: 5)
+                    
+                    Button {
+                        guard !textFieldText.isEmpty else { return }
+                        vm.addFruit(text: textFieldText)
+                        textFieldText = ""
+                    } label: {
+                        Text("Submit")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .shadow(color: .gray, radius: 5, x: 5, y: 5)
+                }
+                .padding(.horizontal)
+
+                
+                List {
+                    ForEach(vm.savedEntitties) { entity in
+                        Text(entity.name ?? "")
+                            .onTapGesture {
+                                vm.updateFruit(entity: entity)
+                            }
+                    }
+                    .onDelete(perform: vm.deleteFruit)
+                }
+                .listStyle(PlainListStyle())
+              
+            }
+            .navigationTitle("Fruits")
+    }
+}
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
